b297385ae81378329eefb2438ee9cf6d8c5d531e
hasbi256 <hasbiashidiqie10@gmail.com>
2026-09-23 20:36:00 +0800

    menyimpan

---
+++ daftar mata kuliah.py
@@ -0,0 +1,13 @@
+daftar_matkul = [
+    "Matematika Dasar",
+    "Fisika Umum",
+    "Algoritma dan Pemrograman",
+    "Struktur Data",
+    "Bahasa Inggris Teknik",
+    "Pendidikan Agama",
+    "Kalkulus 1"
+]
+
+print("Daftar Mata Kuliah:")
+for matkul in daftar_matkul:
+    print(f"- {matkul}")
